bb53d12eb84be27d72b6d98c627b682ccfa2c517
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-08-18 12:40:05 -0700

    Issue#18 add pagination (#22)
    * Added Pagination Logic to Product Controller Index()
    
    * Added page number navigation to catalog page

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -19,17 +19,33 @@ namespace eCommerceSite.Controllers
         }
 
         /// <summary>
-        /// Displays a view that lists all products
+        /// Displays a view that lists a page of products
         /// </summary>
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? id)
         {
-            // Async with Query Syntax
-            //List<Product> products =
-            //    await (from p in _context.Products
-            //           select p).ToListAsync();
+            // can be condensed further! int pageNum = id ?? 1; null coalesing operator ??
+            int pageNum = id.HasValue ? id.Value : 1; // if else ternary operator
+            const int PageSize = 3;
+            ViewData["CurrentPage"] = pageNum;
+
+            int numProducts = await (from p in _context.Products
+                               select p).CountAsync();
+            int totalPages = (int)Math.Ceiling((double)numProducts / PageSize);
+
+            ViewData["MaxPage"] = totalPages;
+
+
+            //Async with Query Syntax
+            List < Product > products =
+                await (from p in _context.Products
+                       orderby p.Title ascending
+                       select p)
+                       .Skip(PageSize * (pageNum - 1))
+                       .Take(PageSize)
+                       .ToListAsync();
 
             // Get all products from DB
-            List<Product> products = await _context.Products.ToListAsync();
+            //List<Product> products = await _context.Products.ToListAsync();
 
             // Send list of products to view to be displayed
             return View(products);

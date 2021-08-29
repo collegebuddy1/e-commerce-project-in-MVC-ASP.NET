3dc7f2b5845966413643ab56b29ab6df9d44349b
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-08-28 19:46:05 -0700

    Issue#27 registration (#30)
    * Created UserAAccount Model.
    
    * Added UserAccount Model to Context and did a Code First migration.
    
    * Created register view model
    
    * Added a Register page and modified Register view model
    
    * Add register user into DB

---
+++ eCommerceSite/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public UserController(ProductContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map Data to user account object !This step is for ViewModels
+                UserAccount account = new UserAccount()
+                {
+                    DateOfBirth = reg.DateOfBirth,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    UserName = reg.UserName
+                };
+                // Add to database
+                _context.UserAccounts.Add(account);
+                await _context.SaveChangesAsync();
+                // Say Hello!
+                TempData["Message"] = $"Hello {reg.UserName}!";
+                // Redirect to home
+                return RedirectToAction("Index", "Home");
+            }
+            return View(reg);
+        }
+    }
+}
---
+++ eCommerceSite/Models/UserAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime? DateOfBirth { get; set; } // Optional
+    }
+
+    public class RegisterViewModel
+    { // Does not go in DB do not add DbSet for this!!
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
+        [Display(Name = "Confirm Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be at least {2} characters.")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)] // Time ignored
+        [Display(Name = "Date of Birth")]
+        public DateTime? DateOfBirth { get; set; } // Optional
+    }
+}
